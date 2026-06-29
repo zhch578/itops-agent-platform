ba442fc52328df691ccd8ca01ca2fd7877045a14
谭策 <huawei_network@foxmail.com>
2026-06-30 05:20:20 +0800

    chore: 从仓库移除 scripts/ 开发辅助脚本

--- scripts/list_frontend.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-for root, dirs, files in os.walk('frontend/src/modules'):
-    for f in files:
-        if f.endswith(('.ts', '.tsx')):
-            print(os.path.join(root, f))
